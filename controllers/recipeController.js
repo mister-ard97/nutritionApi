eb49d723246e7f639575ab87552eac5467b4c758
Enverd <yuvensenverd@yahoo.com>
2019-10-21 12:49:35 +0700

    asdas

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -12,10 +12,34 @@ module.exports = {
         })  
     },  
     postRecipeData  : (req,res) =>{
-        console.log(req.body)
+        // req.body = ['namarecipe', [ {}, {}, {} ]]  {} >> itemId, gram
+        console.log(req.body[0])
+        var datarecipe = {
+            recipeName : req.body[0]
+        }
         var sql = `INSERT INTO recipes set ?`
-        db.query(sql,req.body, (err,result)=>{
+        db.query(sql,datarecipe, (err,result)=>{
             if(err) return res.status(500).send({ message: "Error :(", error: err})
+
+            var recipeid = result.insertId
+            // {
+            //     itemId,
+            //     gram,
+            //     recipeid
+            // }
+            for(var i = 0 ; i<req.body[1].length; i++){
+                var data = {...req.body[1][i], recipeid }
+                console.log(data)
+                db.query(sql,data, (err,result)=>{
+                    if(err) return res.status(500).send({ message: "Error :(", error: err})
+
+                    console.log('details ' + i + 'success')
+            
+    
+                })  
+            }
+
+     
     
             return res.status(200).send(result)
         })  
@@ -44,6 +68,19 @@ module.exports = {
             return res.status(200).send(result)
         })  
     },
+    searchRecipeName : (req,res)=>{
+        console.log(req.params.name)
+        var sql = `select * from recipes where recipeName like '%${req.params.name}%'`
+        db.query(sql,req.body, (err,result)=>{
+            if(err) return res.status(500).send({ message: "Error :(", error: err})
+            
+
+            console.log("find recipe berhasil")
+            console.log('found '+ result.length + ' Products' )
+            return res.status(200).send(result)
+        })  
+    },
+
     deleteRecipeData : (req,res) =>{
         console.log(req.params.id)
         var sql = `delete from recipes where id = ${req.params.id}`
