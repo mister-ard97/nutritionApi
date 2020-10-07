c683f0e2a8cdb4815396f288f5b1f2f76d9b486f
mister-ard97 <rezardiansyah1997@gmail.com>
2020-10-07 08:09:58 +0700

    update_api

--- routers/recipeRouter.js
+++ routers/recipeRouter.js
@@ -1,17 +1,16 @@
-const express = require('express')
-const { recipeController } = require('../controllers')
-const router = express.Router()
+const express = require("express");
+const { recipeController } = require("../controllers");
+const router = express.Router();
 
-router.get('/getrecipe', recipeController.getRecipeData)
-router.post('/postrecipe', recipeController.postRecipeData)
-router.put('/editrecipe', recipeController.editRecipeData)
-router.put('/editrecipedetail/:id', recipeController.editRecipeDetail);
-router.get('/deleterecipedetail/:id', recipeController.deleteRecipeDetail)
-router.get('/deleterecipe/:id', recipeController.deleteRecipeData)
-router.get('/searchrecipe/:name', recipeController.searchRecipeName);
-router.get('/getrecipedetail/:id', recipeController.getDetailRecipe);
+router.get("/getrecipe", recipeController.getRecipeData);
+router.post("/postrecipe", recipeController.postRecipeData);
+router.put("/editrecipe/:id", recipeController.editRecipeData);
+router.put("/editrecipedetail/:id", recipeController.editRecipeDetail);
+router.get("/deleterecipedetail/:id", recipeController.deleteRecipeDetail);
+router.get("/deleterecipe/:id", recipeController.deleteRecipeData);
+router.get("/searchrecipe/:name", recipeController.searchRecipeName);
+router.get("/getrecipedetail/:id", recipeController.getDetailRecipe);
 
-router.post('/editRecipeDetailItem/:id', recipeController.editRecipeDetailItem)
+router.post("/editRecipeDetailItem/:id", recipeController.editRecipeDetailItem);
 
-
-module.exports = router
+module.exports = router;
